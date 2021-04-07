7bb1b2822c76a3a4fd3827892b8c53b40172cb4c
Oscar Nunez <noscar07@gmail.com>
2021-04-07 00:08:43 -0400

    made the card and deck class

--- code.py
+++ code.py
@@ -5,3 +5,33 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
          'Queen':10, 'King':10, 'Ace':11}
 playing = True
 
+class Card:
+
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank 
+
+    def __str__(self):
+        return self.rank+ ' of ' +self.suit
+
+class Deck:
+    #empty list
+    def __init__(self):
+
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n'+ card.__str__()
+        return 'The deck has: ' + deck_comp
+
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
